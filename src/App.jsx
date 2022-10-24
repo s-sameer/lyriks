9011350d8b14b42924de3f7d47a004fbb1926a67
s-sameer <itsnotsameer@gmail.com>
2022-10-24 12:58:22 -0230

    removed sticky

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
               <Route path="/search/:searchTerm" element={<Search />} />
             </Routes>
           </div>
-          <div className="xl:sticky relative top-0 h-fit">
+          <div className="h-fit">
             <TopPlay />
           </div>
         </div>
